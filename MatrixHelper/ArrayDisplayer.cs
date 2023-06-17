11c7dcc55c6550e569ace736460d585ee2d61630
fishd <fishd@SPACETOADS>
2023-06-17 15:02:26 -0400

    Projektdateien hinzufügen.

---
+++ MatrixHelper/ArrayDisplayer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+class Matrix
+{
+    int m;
+    int n;
+    double[,] matrix;
+
+    public Matrix(int numRows, int numCols)
+    {
+        m = numRows;
+        n = numCols;
+        matrix = new double[m, n];
+    }
+
+    // Displays the entire matrix to the user
+    public void Display()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        int counter = 1;
+        Console.Write("\n|    ");
+
+        foreach(double entry in matrix)
+        {
+            int numSpaces = 5 - entry.ToString().Length; // Take into account how we space our entries depending on their size to neatly display our matrix
+
+            if (numSpaces <= 0)
+            {
+                numSpaces = 4;
+
+            }
+           
+            string repeatedSpace = new string(' ', numSpaces); 
+            if(counter > n) // We've reached the end of a row 
+            {
+                Console.Write("|\n\n|    ");
+                counter = 1;
+            }
+
+            Console.Write($"{entry}" + repeatedSpace);
+            counter++;
+        }
+
+        Console.WriteLine("|");
+        Console.ResetColor();
+    }
+
+    // Lets the user input entries into their matrix row by row 
+    public void EntryInput()
+    {
+        Console.WriteLine("\nInsert your entries row by row, separated by a space (decimals are OK).");
+        Console.WriteLine("To exit early, enter 'exit'. To skip to the next row instead, enter 'skip'.\n");
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            Console.ResetColor();
+            Console.Write($"ROW {row + 1}: ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            string userInput = Console.ReadLine()!.Trim().ToLower();
+            Console.ResetColor();
+            
+            if(String.IsNullOrEmpty(userInput))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Entries.");
+                row--;
+                continue;
+            }
+
+            else if(userInput == "exit")
+            {
+                break;
+            }
+
+            else if(userInput == "skip")
+            {
+                continue;
+            }
+
+            string[] entriesArr = userInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // Remove the extra spaces in between
+            int currentCol = 0;
+            double num;
+
+            foreach (string entry in entriesArr)
+            {
+                if(entry == "")
+                {
+                    continue;
+                }
+
+                if(!double.TryParse(entry, out num) || entriesArr.Length > matrix.GetLength(0) || entriesArr.Length < matrix.GetLength(0))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid entries / entries are too big");
+                    row--;
+                    break;
+                }
+
+                matrix[row, currentCol] = num;
+                currentCol++;
+            }
+        }
+    }
+
+    public void Scale()
+    {
+        Console.ResetColor();
+        double scalar;
+
+        Console.Write("\nInsert the scaling factor to multiply your matrix with: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        string userInput = Console.ReadLine()!.Trim();
+        Console.ResetColor();
+
+        if (string.IsNullOrEmpty(userInput) || !double.TryParse(userInput, out scalar))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Scaling Factor.");
+            return;
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+
+                if (double.IsInfinity(matrix[i, j] * scalar))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Chosen scaling factor is too large, please choose a smaller value.");
+                    return;
+                    
+                }
+
+                matrix[i, j] = matrix[i, j] * scalar;
+            }
+        }
+    }
+}
+ 
---
+++ MatrixHelper/Program.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+
+Console.ResetColor();
+Console.WriteLine("\nWelcome to the Matrix Calculator! To get started, input the number of rows and columns you'd like for your matrix!");
+Console.WriteLine("Matrices of up to 10 by 10 size are possible. Make sure your inputs are integers when setting up your matrix size.");
+
+string desiredRows;
+string desiredCols;
+int numRows;
+int numCols;
+Matrix myMatrix;
+
+// Setting up the user's matrix
+while(true)
+{
+    Console.ResetColor();
+
+    Console.Write("\nNumber of Rows (Vertical): ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    desiredRows = Console.ReadLine()!.Trim();
+    Console.ResetColor();
+
+    if (string.IsNullOrEmpty(desiredRows) || !int.TryParse(desiredRows, out numRows))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid Input.");
+        continue;
+    }
+
+    if(numRows > 10 || numRows <= 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Input must be greater than 0 and less than 10, please try again.");
+        continue;
+    }
+
+    Console.Write("Number of Columns (Horizontal): ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    desiredCols = Console.ReadLine()!.Trim();
+    Console.ResetColor();
+
+    if (string.IsNullOrEmpty(desiredCols) || !int.TryParse(desiredCols, out numCols))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid Input.");
+        continue;
+    }
+
+    if (numCols > 10 || numCols <= 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Input must be greater than 0 and less than 10, please try again.");
+        continue;
+    }
+
+    myMatrix = new Matrix(numRows, numCols);
+    myMatrix.EntryInput();
+    break; 
+}
+
+string desiredCommand;
+int userCommand;
+
+while(true)
+{
+    myMatrix.Display();
+    Console.WriteLine("\nPlease input one of the given numbers to perform a command on your matrix:");
+    Console.WriteLine("1. Reinput Your Entries\n2. Row Reduce\n3. Perform Elementary Operations\n4. Invert the Matrix\n5. Find the Determinant\n" +
+        "6. Scale Your Matrix by a Factor\n7. Clear Your Matrix\n" +
+        "8. Make a New Matrix (Discard current one)\n9. Quit the Program");
+    Console.ResetColor();
+
+    Console.Write("\nYour Command Number: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    desiredCommand = Console.ReadLine()!.Trim();
+    Console.ResetColor();
+
+    if(String.IsNullOrEmpty(desiredCommand) || !int.TryParse(desiredCommand, out userCommand) || userCommand > 11 || userCommand <= 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid Command.");
+        continue;
+    }
+
+    switch(userCommand)
+    {
+        case 1:
+            myMatrix.EntryInput();
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        case 5:
+            break;
+
+        case 6:
+            myMatrix.Scale();
+            break;
+
+        case 7:
+            break;
+        case 8:
+            System.Environment.Exit(0);
+            break;
+    }
+}
+
